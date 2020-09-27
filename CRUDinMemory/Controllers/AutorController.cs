c8f12078ca5019f8ef9a6be2fe7fbc2449fb37fa
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2020-09-27 19:31:05 -0300

    Etapa 05 - Arquitetura modificada.

--- CRUDinMemory/Controllers/AutorController.cs
+++ CRUDinMemory/Controllers/AutorController.cs
@@ -1,39 +1,45 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Threading.Tasks;
-using CRUDinMemory.Models;
+﻿using CRUDinMemory.Models;
+using CRUDinMemory.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRUDinMemory.Controllers
 {
     public class AutorController : Controller
     {
+        private readonly AutorRepository _autorRepository;
+
+        public AutorController()
+        {
+            _autorRepository = new AutorRepository();
+        }
         //Coleção em memória - Estado global por ser static
         // public static List<AutorModel> Autores { get; } = new List<AutorModel>();//Lista não é thead safe, pois pode haver concorrência
-        public static ConcurrentDictionary<int,AutorModel> Autores { get; } = new ConcurrentDictionary<int, AutorModel>();
+        //public static Dictionary<int,AutorModel> Autores { get; } = new Dictionary<int, AutorModel>();
         // List
         public IActionResult Index() 
         {
-            var autores = Autores.Select(x=>x.Value);
-            return View(autores);
+            //var autores = Autores.Select(x=>x.Value);
+            //return View(autores);
+            
+            return View(_autorRepository.GetAll());
         }
 
         //Details
         public IActionResult Details(int id)
         {
-            if (Autores.TryGetValue(id, out var autorModel))
-            {
-                return View(autorModel);
-            }
+            return View(_autorRepository.GetById(id));
 
-            return RedirectToAction(nameof(Index));
-        
-            
 
-            
+            //if (Autores.TryGetValue(id, out var autorModel))
+            //{
+            //    return View(autorModel);
+            //}
+
+            //return RedirectToAction(nameof(Index));
+
+
+
+
             //Com Lambda
             //var autor = Autores.FirstOrDefault(x=>x.Id == id);
             //if(autor != null)
@@ -44,7 +50,7 @@ namespace CRUDinMemory.Controllers
             //{
             //    return RedirectToAction(nameof(Index));
             //}
-            
+
         }
 
         //Create
@@ -53,7 +59,9 @@ namespace CRUDinMemory.Controllers
         [HttpPost]
         public IActionResult Create(AutorModel autorModel)
         {
-            Autores.AddOrUpdate(autorModel.Id, autorModel,(key, model)=>autorModel);
+
+            _autorRepository.Add(autorModel);
+            //Autores.AddOrUpdate(autorModel.Id, autorModel,(key, model)=>autorModel);
             return RedirectToAction(nameof(Index));
         }
 
@@ -61,29 +69,36 @@ namespace CRUDinMemory.Controllers
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            //View(Autores.First(x => x.Id == id));
-            if (Autores.TryGetValue(id, out var autorModel))
-            {
-                return View(autorModel);
-            }
 
-            return RedirectToAction(nameof(Index));
+            return View(_autorRepository.GetById(id));
+            //View(Autores.First(x => x.Id == id));
+            //if (Autores.TryGetValue(id, out var autorModel))
+            //{
+            //    return View(autorModel);
+            //}
+
+            //return RedirectToAction(nameof(Index));
         }
         [HttpPost]
         public IActionResult Edit(AutorModel autorModel)
         {
+            _autorRepository.Edit(autorModel);         
+
+            return RedirectToAction(nameof(Index));
             // var autorToEdit = Autores.First(x => x.Id == autorModel.Id);
-            if (Autores.TryGetValue(autorModel.Id, out var autorToEdit))
-            {
-                autorToEdit.Nome = autorModel.Nome;
-                autorToEdit.UltimoNome = autorModel.UltimoNome;
-                autorToEdit.Nascimento = autorModel.Nascimento;
-                return RedirectToAction(nameof(Index));
-            }
+            //if (Autores.TryGetValue(autorModel.Id, out var autorToEdit))
+            //{
+            //    autorToEdit.Nome = autorModel.Nome;
+            //    autorToEdit.UltimoNome = autorModel.UltimoNome;
+            //    autorToEdit.Nascimento = autorModel.Nascimento;
+            //    return RedirectToAction(nameof(Index));
+            //}
+
+
 
             //ModelState.AddModelError(string.Empty, "Id não encontrado em memória");
-            ModelState.AddModelError(nameof(AutorModel.Id), "Id não encontrado em memória");
-            return View(autorModel);
+            //ModelState.AddModelError(nameof(AutorModel.Id), "Id não encontrado em memória");
+            //return View(autorModel);
 
         }
 
@@ -91,20 +106,23 @@ namespace CRUDinMemory.Controllers
         [HttpGet]
         public IActionResult Delete(int id)
         {
+            return View(_autorRepository.GetById(id));
             // View(Autores.First(x => x.Id == id));
-            if (Autores.TryGetValue(id, out var autorModel))
-            {
-                return View(autorModel);
-            }
+            //if (Autores.TryGetValue(id, out var autorModel))
+            //{
+            //    return View(autorModel);
+            //}
 
-            return RedirectToAction(nameof(Index));
+            //return RedirectToAction(nameof(Index));
         }
         [HttpPost]
         public IActionResult Delete(AutorModel autorModel)
         {
-            //var autorToRemove = Autores.FirstOrDefault(x => x.Id == autorModel.Id);          
-            Autores.TryRemove(autorModel.Id, out var autorRemoved);
+            _autorRepository.Remove(autorModel);
             return RedirectToAction(nameof(Index));
+            ////var autorToRemove = Autores.FirstOrDefault(x => x.Id == autorModel.Id);          
+            //Autores.TryRemove(autorModel.Id, out var autorRemoved);
+            //return RedirectToAction(nameof(Index));
         }
 
     }
---
+++ Data/Repositories/LivroRepository.cs
@@ -0,0 +1,39 @@
+﻿using CRUDinMemory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CRUDinMemory.Repositories
+{
+    public class LivroRepository
+    {
+        public static List<LivroModel> Livros { get; } = new List<LivroModel>();
+
+        public IEnumerable<LivroModel> GetAll()
+        {
+            return Livros;
+        }
+        public LivroModel GetById(int id)
+        {
+            var livro = Livros.First(x => x.Id == id);
+            return livro;
+        }
+        public void Add(LivroModel livroModel)
+        {
+            Livros.Add(livroModel);
+        }
+        public void Remove(LivroModel livroModel)
+        {
+            var livroInMemory = GetById(livroModel.Id);
+            Livros.Remove(livroInMemory);
+        }
+        public void Edit(LivroModel livroModel)
+        {
+            var livroInMemory = GetById(livroModel.Id);
+            livroInMemory.Titulo = livroModel.Titulo;
+            livroInMemory.Isbn = livroModel.Isbn;
+            livroInMemory.Publicacao = livroModel.Publicacao;
+            livroInMemory.Autor = livroModel.Autor;
+            livroInMemory.AutorId = livroModel.AutorId;
+        }
+    }
+}
