9ebb745fd59355e26a2b0d933fefae9d49928069
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2020-09-27 16:48:18 -0300

    Etapa 04 ConcurrentDictionary para Autores - Livro controller Comentado

--- CRUDinMemory/Controllers/AutorController.cs
+++ CRUDinMemory/Controllers/AutorController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
@@ -11,26 +12,38 @@ namespace CRUDinMemory.Controllers
     public class AutorController : Controller
     {
         //Coleção em memória - Estado global por ser static
-        public static List<AutorModel> Autores { get; } = new List<AutorModel>();//Lista não é thead safe, pois pode haver concorrência
-
+        // public static List<AutorModel> Autores { get; } = new List<AutorModel>();//Lista não é thead safe, pois pode haver concorrência
+        public static ConcurrentDictionary<int,AutorModel> Autores { get; } = new ConcurrentDictionary<int, AutorModel>();
         // List
         public IActionResult Index() 
         {
-            return View(Autores);
+            var autores = Autores.Select(x=>x.Value);
+            return View(autores);
         }
 
         //Details
         public IActionResult Details(int id)
         {
-            var autor = Autores.FirstOrDefault(x=>x.Id == id);
-            if(autor != null)
+            if (Autores.TryGetValue(id, out var autorModel))
             {
-                return View(autor);
+                return View(autorModel);
             }
-            else
-            {
-                return RedirectToAction(nameof(Index));
-            }
+
+            return RedirectToAction(nameof(Index));
+        
+            
+
+            
+            //Com Lambda
+            //var autor = Autores.FirstOrDefault(x=>x.Id == id);
+            //if(autor != null)
+            //{
+            //    return View(autor);
+            //}
+            //else
+            //{
+            //    return RedirectToAction(nameof(Index));
+            //}
             
         }
 
@@ -40,33 +53,57 @@ namespace CRUDinMemory.Controllers
         [HttpPost]
         public IActionResult Create(AutorModel autorModel)
         {
-            Autores.Add(autorModel);
+            Autores.AddOrUpdate(autorModel.Id, autorModel,(key, model)=>autorModel);
             return RedirectToAction(nameof(Index));
         }
 
         //Edit
         [HttpGet]
-        public IActionResult Edit(int id) => View(Autores.First(x=>x.Id==id));
+        public IActionResult Edit(int id)
+        {
+            //View(Autores.First(x => x.Id == id));
+            if (Autores.TryGetValue(id, out var autorModel))
+            {
+                return View(autorModel);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
         [HttpPost]
         public IActionResult Edit(AutorModel autorModel)
         {
-            var autorToEdit = Autores.First(x => x.Id == autorModel.Id);
+            // var autorToEdit = Autores.First(x => x.Id == autorModel.Id);
+            if (Autores.TryGetValue(autorModel.Id, out var autorToEdit))
+            {
+                autorToEdit.Nome = autorModel.Nome;
+                autorToEdit.UltimoNome = autorModel.UltimoNome;
+                autorToEdit.Nascimento = autorModel.Nascimento;
+                return RedirectToAction(nameof(Index));
+            }
 
-            autorToEdit.Nome = autorModel.Nome;
-            autorToEdit.UltimoNome = autorModel.UltimoNome;
-            autorToEdit.Nascimento = autorModel.Nascimento;
+            //ModelState.AddModelError(string.Empty, "Id não encontrado em memória");
+            ModelState.AddModelError(nameof(AutorModel.Id), "Id não encontrado em memória");
+            return View(autorModel);
 
-            return RedirectToAction(nameof(Index));
         }
 
         //Delete
         [HttpGet]
-        public IActionResult Delete(int id) => View(Autores.First(x => x.Id == id));
+        public IActionResult Delete(int id)
+        {
+            // View(Autores.First(x => x.Id == id));
+            if (Autores.TryGetValue(id, out var autorModel))
+            {
+                return View(autorModel);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
         [HttpPost]
         public IActionResult Delete(AutorModel autorModel)
         {
-            var autorToRemove = Autores.FirstOrDefault(x => x.Id == autorModel.Id);
-            Autores.Remove(autorToRemove);
+            //var autorToRemove = Autores.FirstOrDefault(x => x.Id == autorModel.Id);          
+            Autores.TryRemove(autorModel.Id, out var autorRemoved);
             return RedirectToAction(nameof(Index));
         }
 
--- CRUDinMemory/Controllers/LivroController.cs
+++ CRUDinMemory/Controllers/LivroController.cs
@@ -1,103 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CRUDinMemory.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using CRUDinMemory.Models;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace CRUDinMemory.Controllers
-{
-    public class LivroController : Controller
-    {
-        //Coleção em memória - Estado global por ser static
-        public static List<LivroModel> Livros { get; } = new List<LivroModel>();//Lista não é thead safe, pois pode haver concorrência
+//namespace CRUDinMemory.Controllers
+//{
+//    public class LivroController : Controller
+//    {
+//        //Coleção em memória - Estado global por ser static
+//        public static List<LivroModel> Livros { get; } = new List<LivroModel>();//Lista não é thead safe, pois pode haver concorrência
 
-        // List
-        public IActionResult Index()
-        {
-            return View(Livros);
-        }
+//        // List
+//        public IActionResult Index()
+//        {
+//            return View(Livros);
+//        }
 
-        //Details
-        public IActionResult Details(int id)
-        {
-            var livro = Livros.FirstOrDefault(x => x.Id == id);
+//        //Details
+//        public IActionResult Details(int id)
+//        {
+//            var livro = Livros.FirstOrDefault(x => x.Id == id);
 
-            if (livro != null)
-            {
-                return View(livro);
-            }
-            else
-            {
-                return RedirectToAction(nameof(Index));
-            }
+//            if (livro != null)
+//            {
+//                return View(livro);
+//            }
+//            else
+//            {
+//                return RedirectToAction(nameof(Index));
+//            }
            
-        }
+//        }
 
-        //Create
-        [HttpGet]
-        public IActionResult Create() => View();
-        [HttpPost]
-        public IActionResult Create(LivroModel livroModel)
-        {
-            Livros.Add(livroModel);
-            if (livroModel.AutorId > 0)
-            {
-                var autor = AutorController.Autores.First(x=>x.Id == livroModel.AutorId);
-                autor.Livros.Add(livroModel);
-                livroModel.Autor = autor;
+//        //Create
+//        [HttpGet]
+//        public IActionResult Create() => View();
+//        [HttpPost]
+//        public IActionResult Create(LivroModel livroModel)
+//        {
+//            Livros.Add(livroModel);
+//            if (livroModel.AutorId > 0)
+//            {
+//                var autor = AutorController.Autores.First(x=>x.Id == livroModel.AutorId);
+//                autor.Livros.Add(livroModel);
+//                livroModel.Autor = autor;
                 
-            }
-            return RedirectToAction(nameof(Index));
-        }
+//            }
+//            return RedirectToAction(nameof(Index));
+//        }
 
-        //Edit
-        [HttpGet]
-        public IActionResult Edit(int id) => View(Livros.First(x => x.Id == id));
-        [HttpPost]
-        public IActionResult Edit(LivroModel livroModel)
-        {
-            var livroToEdit = Livros.First(x => x.Id == livroModel.Id);
+//        //Edit
+//        [HttpGet]
+//        public IActionResult Edit(int id) => View(Livros.First(x => x.Id == id));
+//        [HttpPost]
+//        public IActionResult Edit(LivroModel livroModel)
+//        {
+//            var livroToEdit = Livros.First(x => x.Id == livroModel.Id);
 
-            livroToEdit.Titulo = livroModel.Titulo;
-            livroToEdit.Isbn = livroModel.Isbn;
-            livroToEdit.Publicacao = livroModel.Publicacao;
+//            livroToEdit.Titulo = livroModel.Titulo;
+//            livroToEdit.Isbn = livroModel.Isbn;
+//            livroToEdit.Publicacao = livroModel.Publicacao;
 
-            if (livroToEdit.AutorId != livroModel.AutorId)
-            {
-                var autor = AutorController.Autores.First(x => x.Id == livroModel.AutorId);
-                var autorAntigo = AutorController.Autores.First(x => x.Id == livroToEdit.AutorId);
+//            if (livroToEdit.AutorId != livroModel.AutorId)
+//            {
+//                var autor = AutorController.Autores.First(x => x.Id == livroModel.AutorId);
+//                var autorAntigo = AutorController.Autores.First(x => x.Id == livroToEdit.AutorId);
 
-                //Inserção do livro para autor novo
-                autor.Livros.Add(livroToEdit);
+//                //Inserção do livro para autor novo
+//                autor.Livros.Add(livroToEdit);
 
-                //Remoção do livro para autor antigo
-                var livroParaRemover = autorAntigo.Livros.First(x=>x.Id==livroToEdit.Id);
-                autorAntigo.Livros.Remove(livroParaRemover);
+//                //Remoção do livro para autor antigo
+//                var livroParaRemover = autorAntigo.Livros.First(x=>x.Id==livroToEdit.Id);
+//                autorAntigo.Livros.Remove(livroParaRemover);
 
-                livroToEdit.Autor = autor;
-                livroToEdit.AutorId = autor.Id;
-            }
+//                livroToEdit.Autor = autor;
+//                livroToEdit.AutorId = autor.Id;
+//            }
 
-            return RedirectToAction(nameof(Index));
-        }
+//            return RedirectToAction(nameof(Index));
+//        }
 
-        //Delete
-        [HttpGet]
-        public IActionResult Delete(int id) => View(Livros.First(x => x.Id == id));
-        [HttpPost]
-        public IActionResult Delete(LivroModel livroModel)
-        {
-            var livroToRemove = Livros.FirstOrDefault(x => x.Id == livroModel.Id);
-            Livros.Remove(livroToRemove);
+//        //Delete
+//        [HttpGet]
+//        public IActionResult Delete(int id) => View(Livros.First(x => x.Id == id));
+//        [HttpPost]
+//        public IActionResult Delete(LivroModel livroModel)
+//        {
+//            var livroToRemove = Livros.FirstOrDefault(x => x.Id == livroModel.Id);
+//            Livros.Remove(livroToRemove);
 
-            if(livroModel.AutorId > 0)
-            {
-                var autor = AutorController.Autores.First(x=>x.Id == livroModel.AutorId);
-                autor.Livros.Remove(livroModel);
-            }
-            return RedirectToAction(nameof(Index));
-        }
+//            if(livroModel.AutorId > 0)
+//            {
+//                var autor = AutorController.Autores.First(x=>x.Id == livroModel.AutorId);
+//                autor.Livros.Remove(livroModel);
+//            }
+//            return RedirectToAction(nameof(Index));
+//        }
 
-    }
-}
+//    }
+//}
